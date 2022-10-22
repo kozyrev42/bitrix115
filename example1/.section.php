b2e9d8007a4a9247758e1a574f51e14967d277a8
kozyrev42 <kozyrev054@gmail.com>
2022-10-22 12:57:03 +0700

    встроил бутстраповский шаблон

---
+++ example1/.section.php
@@ -0,0 +1,7 @@
+<?
+$sSectionName = "example1";
+$arDirProperties = Array(
+   "description" => "это example1 из -свойств раздела-",
+   "keywords" => "КЛЮЧЕВЫЕ СЛОВА из -свойств раздела-"
+);
+?>
---
+++ local/templates/blog/descrition.php
@@ -0,0 +1,9 @@
+<?php
+/* описание шаблона, для подключения через админку */
+$arTemplate = array (
+  'NAME' => 'blog',
+  'DESCRIPTION' => 'описание_blog',
+  'SORT' => '',
+  'TYPE' => '',
+);
+?>
