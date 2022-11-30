505bd7d3de11a5439fe17476f5cad5e11e9bb223
kozyrev42 <kozyrev054@gmail.com>
2022-11-30 23:03:09 +0700

    авторизация в работе, почти готова

--- auth/index.php
+++ auth/index.php
@@ -2,7 +2,8 @@
 define("NEED_AUTH", true);
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Login");
-?><?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "login", Array(
+?>
+<?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "login", Array(
 	"FORGOT_PASSWORD_URL" => "/auth/",	// Страница забытого пароля
 		"PROFILE_URL" => "/auth/profile.php",	// Страница профиля
 		"REGISTER_URL" => "/auth/registration.php",	// Страница регистрации
